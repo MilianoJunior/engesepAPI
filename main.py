afe585cc89713d794f8d043ea4aa764c880b9abb
MilianoJunior <jrmfilho37@gmail.com>
2023-08-29 15:00:39 -0300

    test connection

--- main.py
+++ main.py
@@ -20,36 +20,36 @@ config = {
     'host': host,
     'database': database
 }
-print('Informações de conexão:')
-print('URL: ',mysql_url)
-print('user: ',user)
-print('password: ',password)
-print('host: ',host)
-print('port: ',port)
-print('database: ',database)
-print('-----------------------')
-url = f'mysql://{user}:{password}@{host}:{port}/{database}'
+print('-------------')
+print(config)
+
+try:
+    engine = mysql.connector.connect(**config)
+except Exception as e:
+    df = f'Erro na conexão com o banco de dados 1:{str(e)}'
+print(engine)
+print('-------------')
+
 
 app = FastAPI()
 @app.get("/")
 async def root():
     df = ''
-    try:
-        engine = mysql.connector.connect(**config)
-    except Exception as e:
-        df = f'Erro na conexão com o banco de dados 1:{str(e)}'
+    # try:
+    #     engine = mysql.connector.connect(**config)
+    # except Exception as e:
+    #     df = f'Erro na conexão com o banco de dados 1:{str(e)}'
 
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!", 'df': df}
 
 
 @app.get("/consulta_id/{id}")
 async def consulta_id(id: int):
-    try:
-        engine = create_engine(mysql_url)
-
-        df = pd.read_sql(f'SELECT * FROM cgh_fae WHERE id = {id}', con=engine)
-        return df.to_json()
-    except Exception as e:
-        query = f'SELECT * FROM cgh_fae WHERE id = {id}'
-        return {'error': e,
-                'query': query}
+    # try:
+    #     engine = create_engine(mysql_url)
+    #
+    #     df = pd.read_sql(f'SELECT * FROM cgh_fae WHERE id = {id}', con=engine)
+    #     return df.to_json()
+    # except Exception as e:
+    #     query = f'SELECT * FROM cgh_fae WHERE id = {id}'
+    return {'error': e,'query': query}
