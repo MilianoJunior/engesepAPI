7cb7621e7a4a55119e9fe9d5fa10ad7c98377d55
MilianoJunior <jrmfilho37@gmail.com>
2023-08-29 11:59:42 -0300

    test connection

--- main.py
+++ main.py
@@ -4,14 +4,35 @@ import pandas as pd
 import os
 app = FastAPI()
 
+# mysql+mysqlconnector://<user>:<password>@<host>[:<port>]/<dbname>
 mysql_url = os.getenv('MYSQL_URL')
+user  = os.getenv('MYSQLUSER')
+password = os.getenv('MYSQLPASSWORD')
+host = os.getenv('MYSQLHOST')
+port = os.getenv('MYSQLPORT')
+database = os.getenv('MYSQLDATABASE')
+print('Informações de conexão:')
 print('URL: ',mysql_url)
+print('user: ',user)
+print('password: ',password)
+print('host: ',host)
+print('port: ',port)
+print('database: ',database)
+print('-----------------------')
+url = f'mysql://{user}:{password}@{host}:{port}/{database}'
+
 @app.get("/")
 async def root():
     try:
-        engine = create_engine(mysql_url)
+        # tratamento de erro da conexão
+        try:
+            engine = create_engine(mysql_url)
+        except Exception as e:
+            df = {'error 1': e}
+
         df = pd.read_sql(f'SELECT * FROM cgh_fae WHERE id = {id}', con=engine).to_json()
     except Exception as e:
+
         df = {'error': e}
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!", 'df': df}
 
