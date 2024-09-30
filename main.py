89848e867b8a48410013c5e83ddb352213ab3b91
MilianoJunior <jrmfilho37@gmail.com>
2024-09-30 08:22:42 -0300

    add cors

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import requests
 import json
 import os
 import time
+from fastapi.middleware.cors import CORSMiddleware
 # from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime, timedelta
 import threading
@@ -24,6 +25,21 @@ app = FastAPI(
     title="ENGESEP",
     description="Documentação da API ENGESEP",
     version="1.0.0",
+)
+
+# configurar o CORS
+origins = [
+    "http://localhost",
+    "http://0.0.0.0",
+    "fastapi-production-8d7e.up.railway.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 '''
@@ -47,30 +63,6 @@ app.post("/columns")(rotas.get_columns)
 ''' Endpoint para obter os dados da produção total de energia '''
 app.post("/data/producao_total")(rotas.get_production_all)
 
-# def evento():
-#     ''' Função para enviar email todos os dias as 08:00 '''
-#
-#     # importar a função main do arquivo email.py -  A lógica está testada e enviando email corretamente
-#     from libs.email import main
-#     print('Evento disparado as 15:10')
-#
-#     main()
-
-# def start_scheduler():
-#     ''' Iniciar o agendador APScheduler '''
-#     scheduler = BackgroundScheduler()
-#     # Agendar a função evento para ser executada todos os dias às 08:00
-#     scheduler.add_job(evento, 'cron', hour=15, minute=22)
-#     scheduler.start()
-#
-#     # Garantir que o agendador pare ao desligar o programa
-#     try:
-#         while True:
-#             time.sleep(2)
-#     except (KeyboardInterrupt, SystemExit):
-#         scheduler.shutdown()
-
-
 
 # 12 - Iniciar o servidor FastAPI
 def run_uvicorn():
@@ -78,10 +70,6 @@ def run_uvicorn():
 
     # ler a variável de ambiente HOST
     host = os.getenv("HOST", '0.0.0.0')
-
-    # # iniciar agendador de tarefas em um thread separado
-    # scheduler_thread = threading.Thread(target=start_scheduler)
-    # scheduler_thread.start()
 
     print('Servidor iniciado')
 
